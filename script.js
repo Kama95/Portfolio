9df0493fae55f00c682c8dc16d01ffc400c26872
Kama <kamasimbeye89@gmail.com>
2024-06-19 12:47:46 +0200

    Add gradient to page for an animation effect to highlight position of mouse.
    creat gradient div in html.
    style it with css, giving it a position of absolute
    give it a left of -150px and a top of 100px to center
    it on the curser
    
    add an event lstener to listen for a mouse move
    and update the position of the curser
    
    add a loop to animate div as it follows curser

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ $(document).ready(function(){
     });
 }
 );
+
+console.log("Javascrip is working alright");
+
+let mousePosition = {
+    x:0,
+    y:0
+};
+
+document.addEventListener("mousemove", (mouse) =>{
+    mousePosition ={
+        x:mouse.clientX,
+        y:mouse.clientY
+    };
+    console.log (mousePosition);
+
+    
+});
+
+
+const loop = ()=>{
+    const gradientElement = document.getElementById('gradient');
+
+    if (gradientElement){
+        //const rect = gradientElement.getBoundingClientRect();
+        //const elementWidth =rect.width;
+        //const elementHeight=rect.height;
+
+        
+    gradientElement.style.transform =
+    `translate(${mousePosition.x}px, ${mousePosition.y}px)`;
+    }
+        window.requestAnimationFrame(loop);
+};
+
+window.requestAnimationFrame(loop);
