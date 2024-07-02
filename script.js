3ecb93e447b1ff9fc7bcd8d83085677fd369da3e
Kama <kamasimbeye89@gmail.com>
2024-07-02 16:40:37 +0200

    feat: Implement slide navigation logic in showSlides function
    - Add `showSlides` function to display the current
     slide based on the slide index.
    - Retrieve all slide elements using
    `document.getElementsByClassName("mySlides")`.
    - Implement wrap-around logic to ensure
    continuous navigation:
      - Reset `slideIndex` to 1 if it exceeds the
     total number of slides.
      - Set `slideIndex` to the last slide if it is
      less than 1.
    - Hide all slides initially by setting their
    `display` property to "none".
    - Display the current slide by setting its
    `display` property to "block".
    
    This update enhances the lightbox functionality
    by allowing users to navigate through slides
    seamlessly, including wrapping around when
    navigating beyond the first or last slide.

--- script.js
+++ script.js
@@ -96,4 +96,10 @@ function currentSlide(n){
 function showSlides(n){
     let i;
     let slides = document.getElementsByClassName('slides');
+    if (n > slides.length){slideIndex=1};
+    if (n < 1){slideIndex = slides.length};
+    for (i=0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex-1].style.display='block';
 }
