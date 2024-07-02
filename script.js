4c7f80ffd1a8e5e862b1629a59bb130b893fae62
Kama <kamasimbeye89@gmail.com>
2024-07-02 16:22:51 +0200

    Create function to make modal appear when view gallery button is pressed. function will set my modal display to block

--- script.js
+++ script.js
@@ -73,3 +73,11 @@ anchor.addEventListener('click' ,function(e)
     });
   });
 });
+
+function openModal(){
+    document.getElementById('myModal').style.display='block';
+}
+
+function closeModal(){
+    document.getElementById('myModal').style.display='none';
+}
